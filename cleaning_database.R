409f644998261ef94690d07e583c05fb087c6757
csoderberg <csoderbe@gmail.com>
2020-11-05 13:21:49 -0500

    output file of eligible preprints

--- cleaning_database.R
+++ cleaning_database.R
@@ -8,3 +8,10 @@ osf_retrieve_file('kba24') %>%
   osf_download()
 
 database_data <- read_csv(here::here('/raw_database_output.csv'))
+
+
+# export csv of pp to include in study
+database_data %>%
+  filter(has_data_links != 'not_applicable') %>%
+  select(guid, date_published) %>%
+  write_csv('eligible_preprints')
