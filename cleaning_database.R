e4cd9f72ade91f84e64adbf476b2a70d849d91cf
csoderberg <csoderbe@gmail.com>
2020-11-05 13:52:27 -0500

    remove unnecessary variable

--- cleaning_database.R
+++ cleaning_database.R
@@ -16,6 +16,6 @@ database_data <- read_csv(here::here('/raw_database_output.csv'))
 # export csv of pp to include in study
 database_data %>%
   filter(has_data_links != 'not_applicable') %>%
-  select(guid, date_published, pp_num) %>%
+  select(guid, date_published) %>%
   distinct(guid, .keep_all = T) %>%
   write_csv('eligible_preprints.csv')
