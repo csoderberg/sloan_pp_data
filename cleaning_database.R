7e6829f57a07ecffa91e54ad5590024ce9184d77
csoderberg <csoderbe@gmail.com>
2020-11-05 13:48:21 -0500

    fix typo and timezone

--- cleaning_database.R
+++ cleaning_database.R
@@ -2,9 +2,12 @@
 library(tidyverse)
 library(osfr)
 library(here)
+library(lubridate)
+
+Sys.setenv(TZ='UTC')
 
 # reading in data
-osf_retrieve_file('kba24') %>%
+osf_retrieve_file('n9x4t') %>%
   osf_download()
 
 database_data <- read_csv(here::here('/raw_database_output.csv'))
@@ -13,6 +16,6 @@ database_data <- read_csv(here::here('/raw_database_output.csv'))
 # export csv of pp to include in study
 database_data %>%
   filter(has_data_links != 'not_applicable') %>%
-  select(guid, date_published) %>%
+  select(guid, date_published, pp_num) %>%
   distinct(guid, .keep_all = T) %>%
-  write_csv('eligible_preprints')
+  write_csv('eligible_preprints.csv')
