a71ab550c221c9ef31f287a73ab5d345b458e03d
csoderberg <csoderbe@gmail.com>
2020-11-11 10:30:08 -0500

    set up 3 models of increasing complexity

--- analyses.R
+++ analyses.R
@@ -2,3 +2,33 @@
 library(tidyverse)
 library(brms)
 
+# basic model (not including provider level)
+brms(download ~ article_doi + data_shown_blinded + has_data_links_blinded + data_shown_blinded * has_data_links_blinded + (1|participant_id) + (1|guid),
+     family = bernoulli(link = 'logit'),
+     warmup = 500,
+     iter = 2000,
+     chains = 2,
+     inits = '0',
+     cores = 2,
+     seed = 1)
+
+# include provider level intercepts
+brms(download ~ article_doi + data_shown_blinded + has_data_links_blinded + data_shown_blinded * has_data_links_blinded + (1|participant_id) + (1|guid) + (1|pp_provider),
+     family = bernoulli(link = 'logit'),
+     warmup = 500,
+     iter = 2000,
+     chains = 2,
+     inits = '0',
+     cores = 2,
+     seed = 2)
+
+# include provider level slopes
+brms(download ~ article_doi + data_shown_blinded + has_data_links_blinded + data_shown_blinded * has_data_links_blinded + 
+       (1|participant_id) + (1|guid) + (data_shown_blinded + has_data_links_blinded + data_shown_blinded * has_data_links_blinded +1|pp_provider),
+     family = bernoulli(link = 'logit'),
+     warmup = 500,
+     iter = 2000,
+     chains = 2,
+     inits = '0',
+     cores = 2,
+     seed = 3)
